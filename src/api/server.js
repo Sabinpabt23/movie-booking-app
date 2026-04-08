2c86a66237786cce30416aeffd21fa2d393137fe
Sabinpabt23 <sabinpant100@gmail.com>
2026-04-08 14:08:36 +0545

    Add Google OAuth routes and Passport config

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const session = require('express-session');  // Move this up
+const session = require('express-session');  
 const passport = require('../config/passport');
 const contactRoutes = require('../routes/contactRoutes');
 const authRoutes = require('../routes/authRoutes');
